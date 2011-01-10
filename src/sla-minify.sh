479469c59c533bb275eff1187835c19ff0f7fd84
Klas Kalaß <klas.r.kalass@googlemail.com>
2011-01-10 23:07:24 +0100

    make target scale configurable

--- src/sla-minify.sh
+++ src/sla-minify.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #
 # Script zum Verkleinern von Bildern in Scribus Dateien, so dass
-# sie nur noch einen Skalierungsfaktor von 0.24 aufweisen
+# sie nur noch den gegebenen Skalierungsfaktor aufweisen
 # 
-
-TARGET_SCALE_FACTOR=0.24
 
 FILENAME=$1
 TARGET_DIR=$2
+TARGET_SCALE_FACTOR=$3
+
 TARGET_IMG_DIR=img
 
-if [ -z "$FILENAME" ] || [ -z "$TARGET_DIR" ] ; then
+if [ -z "$FILENAME" ] || [ -z "$TARGET_DIR" ] || [ -z "$TARGET_SCALE_FACTOR" ]; then
   echo "usage:"
-  echo "$0 <FILENAME> <TARGET_DIR>"
+  echo "$0 <FILENAME> <TARGET_DIR> <TARGET_SCALE_FACTOR>"
   echo ""
+  echo "example: $0 myfile.sla build/minified/ 0.20"
+  echo "  scale: 0.20 ==> 360 dpi"
+  echo "  scale: 0.24 ==> 300 dpi"
   exit -1
 fi
 
@@ -23,15 +26,17 @@ fi
 #
 # $1 - res - The old Resolution
 # $2 - scale - the old scale factor
-# $3 - bcscale - the scale to give to bc
+# $3 - targetscale - the target scale factor
+# $4 - bcscale - the scale to give to bc
 #
 # @return: the new, scaled down resolution
 #
 function scaleResolution {
   local res=$1
   local scale=$2
-  local bcscale=$3
-  echo "scale=$bcscale;($res*${scale})/$TARGET_SCALE_FACTOR" | bc
+  local targetscale=$3
+  local bcscale=$4
+  echo "scale=$bcscale;($res*${scale})/$targetscale" | bc
 }
 
 #
@@ -40,16 +45,17 @@ function scaleResolution {
 # $1 - img - the image to scale
 # $2 - oldScaleX - the old factor in X dimension
 # $3 - oldScaleY - the old factor in Y dimension
-#
+# $4 - targetscale
 function getNewResolution {
   local img="$1"
   local oldScaleX=$2
   local oldScaleY=$3
+  local targetscale=$4
   local resString=`identify "$img"  | cut -f3 -d " "  `
   local oldResX=`echo $resString|cut -d "x" -f1`
-  local newResX=`scaleResolution $oldResX $oldScaleX 0`
+  local newResX=`scaleResolution $oldResX $oldScaleX $targetscale 0`
   local oldResY=`echo $resString|cut -d "x" -f2`
-  local newResY=`scaleResolution $oldResY $oldScaleY 0`
+  local newResY=`scaleResolution $oldResY $oldScaleY $targetscale 0`
   echo ${newResX}x${newResY}+0+0
 }
 
@@ -66,6 +72,7 @@ function getAttribute {
 function minify {
   local filename=$1
   local targetdir=$2
+  local targetscale=$3
   local targetfile="$targetdir/"`basename "$filename"`
   local srcdirname=`dirname "$filename"`
 
@@ -84,17 +91,17 @@ function minify {
       local scaleY=`getAttribute "$line" LOCALSCY`
       local localX=`getAttribute "$line" LOCALX`
       local localY=`getAttribute "$line" LOCALY`
-      local newlocalx=`scaleResolution $localX $scaleX 12`
-      local newlocaly=`scaleResolution $localY $scaleY 12`
-      local resString=`getNewResolution "$srcdirname/$imgfile" $scaleX $scaleY`
+      local newlocalx=`scaleResolution $localX $scaleX $targetscale 12`
+      local newlocaly=`scaleResolution $localY $scaleY $targetscale 12`
+      local resString=`getNewResolution "$srcdirname/$imgfile" $scaleX $scaleY $targetscale`
       local newimagename="$TARGET_IMG_DIR/$resString-"`basename "$imgfile"`
      
       echo "Scaling down $imgfile"
       
       convert -scale "$resString" "$srcdirname/$imgfile" "$targetdir/$newimagename"
 
-      echo $line | sed "s#LOCALSCX=\"[^\"]*\"#LOCALSCX=\"${TARGET_SCALE_FACTOR}\"#g;\
-                        s#LOCALSCY=\"[^\"]*\"#LOCALSCY=\"${TARGET_SCALE_FACTOR}\"#g;\
+      echo $line | sed "s#LOCALSCX=\"[^\"]*\"#LOCALSCX=\"${targetscale}\"#g;\
+                        s#LOCALSCY=\"[^\"]*\"#LOCALSCY=\"${targetscale}\"#g;\
                         s#LOCALX=\"[^\"]*\"#LOCALX=\"${newlocalx}\"#g;\
                         s#LOCALY=\"[^\"]*\"#LOCALY=\"${newlocaly}\"#g;\
                         s#PFILE=\"[^\"]*\"#PFILE=\"${newimagename}\"#g\
@@ -109,4 +116,4 @@ function minify {
 #
 # main
 #
-minify "$FILENAME" "$TARGET_DIR"
+minify "$FILENAME" "$TARGET_DIR" $TARGET_SCALE_FACTOR
